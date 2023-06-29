45c3cef287fc240d87329f0f56d75f85a4e1faae
Tran Nguyen <nguyentc.cmu@vnpt.vn>
2023-06-29 07:25:33 +0700

    add cors

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 SqlService.instance().initialize();
 SocketIOService.instance().initialize(server, {
   cors: {
-    origin: ['https://remi-fe.vercel.app', 'http://localhost:8088', 'http://103.69.193.124:8088'],
+    origin: ['https://remi-fe.vercel.app', 'http://localhost:8088', 'http://103.69.193.117:8088'],
     methods: ['GET', 'POST'], // Allow specific HTTP methods
   }
 });
